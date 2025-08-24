3233b839e12f49de95b927b352838ddca5b145d7
SamgG3 <thinktwice.hsk@gmail.com>
2025-08-24 05:08:03 +0300

    Create logout.js

---
+++ pages/api/auth/logout.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res){
+  res.setHeader("Set-Cookie", [
+    "sess=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
+  ]);
+  res.status(200).json({ ok:true });
+}
