65c53eb82e03a324e866b37994a002ccd015d2a6
SamgG3 <thinktwice.hsk@gmail.com>
2025-08-24 17:13:40 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import dynamic from "next/dynamic";
-import "../styles/globals.css"; // yoksa bu satırı silebilirsin
+// Eğer CSS dosyan yoksa bu satırı silebilirsin:
+try { require("../styles/globals.css"); } catch {}
 
 const TopBar = dynamic(() => import("../components/TopBar"), { ssr: false });
 
@@ -12,3 +13,4 @@ export default function MyApp({ Component, pageProps }) {
     </>
   );
 }
+
