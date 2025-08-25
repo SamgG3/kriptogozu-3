9f7317aef5a4f326f2eb1eadf87d181645f4bc7e
Sam G <thinktwice.hsk@gmail.com>
2025-08-25 23:28:46 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
 // pages/_app.js
-import dynamic from "next/dynamic";
-try { require("../styles/globals.css"); } catch {}
-
-const PriceBar   = dynamic(() => import("../components/PriceBar"),   { ssr:false });
-const TopBar     = dynamic(() => import("../components/TopBar"),     { ssr:false });
+// ...
 const WhaleTicker= dynamic(() => import("../components/WhaleTicker"), { ssr:false });
 
 export default function MyApp({ Component, pageProps }) {
@@ -12,7 +8,8 @@ export default function MyApp({ Component, pageProps }) {
       <PriceBar />
       <TopBar />
       <Component {...pageProps} />
-      <WhaleTicker symbols={["BTCUSDT","ETHUSDT","BNBUSDT"]} bigTradeUsd={200000} />
+      {/* Balina akışı kapalı; bant duruyor */}
+      <WhaleTicker enabled={false} staticText="KriptoGözü • canlı veri • hoş geldiniz" />
     </>
   );
 }
