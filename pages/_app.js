6f6a7d3b63633c0e52a7162e913ad495638b35da
Sam G <thinktwice.hsk@gmail.com>
2025-08-25 22:58:03 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,16 @@
 import dynamic from "next/dynamic";
 try { require("../styles/globals.css"); } catch {}
 
-const TopBar = dynamic(() => import("../components/TopBar"), { ssr: false });
-const WhaleTicker = dynamic(() => import("../components/WhaleTicker"), { ssr: false });
+const PriceBar   = dynamic(() => import("../components/PriceBar"),   { ssr:false });
+const TopBar     = dynamic(() => import("../components/TopBar"),     { ssr:false });
+const WhaleTicker= dynamic(() => import("../components/WhaleTicker"), { ssr:false });
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <PriceBar />
       <TopBar />
       <Component {...pageProps} />
-      {/* Alt bant her sayfada çıksın */}
       <WhaleTicker symbols={["BTCUSDT","ETHUSDT","BNBUSDT"]} bigTradeUsd={200000} />
     </>
   );
