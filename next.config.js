295932ea2d22ba1010358dcb8e3c6ee9518a62a2
Sam G <thinktwice.hsk@gmail.com>
2025-09-03 02:28:22 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: false }
+
+  // Uyarı veriyorsa bunu SİL: experimental: { appDir: true }
+
+  async redirects() {
+    return [
+      { source: "/panel-sinyal", destination: "/sinyal", permanent: false },
+      { source: "/signals",      destination: "/sinyal", permanent: false },
+    ];
+  },
 };
 
 module.exports = nextConfig;
